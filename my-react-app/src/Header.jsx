33c6bd58ac90560b2d0bd9161de487d582103411
Joshua Karasik <joshuakarasik@gmail.com>
2024-10-28 13:25:27 -0700

    more

--- my-react-app/src/Header.jsx
+++ my-react-app/src/Header.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import "./Header.css";
+import PersonIcon from "@mui/icons-material/Person";
+import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
 
 export default function Header() {
   return (
-    <div>
+    <div className="Header">
+      <PersonIcon />
       <h2>I am a Header</h2>
+      <ChatBubbleIcon />
     </div>
   );
 }
