a0e7c35dd4d6aeaa866a28b15754b6b237e86e58
Joshua Karasik <joshuakarasik@gmail.com>
2024-10-28 12:44:54 -0700

    css change

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,7 +2,22 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  /*
+const firebaseConfig = {
+  apiKey: "AIzaSyBvW5yztDZVWLjssuvfY_CWKqpHneXzZ98",
+  authDomain: "clone-47521.firebaseapp.com",
+  projectId: "clone-47521",
+  storageBucket: "clone-47521.appspot.com",
+  messagingSenderId: "1036492225679",
+  appId: "1:1036492225679:web:dc48867a3d27321a4f04fa",
+  measurementId: "G-4ZLL6JB241"
+};
+  */
+  return (
+    <>
+      <h1>Let's build a clone app!</h1>
+    </>
+  );
 }
 
 export default App;
