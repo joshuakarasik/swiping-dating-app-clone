340f3912aa9da8538c83730fe561eee8b80c4a48
Joshua Karasik <joshuakarasik@gmail.com>
2024-10-29 13:52:05 -0700

    buttpns

--- my-react-app/src/SwipeButtons.jsx
+++ my-react-app/src/SwipeButtons.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import "./SwipeButtons.css";
+import ReplayIcon from "@mui/icons-material/Replay";
+import CancelIcon from "@mui/icons-material/Cancel";
+import StarRateIcon from "@mui/icons-material/StarRate";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FlashOnIcon from "@mui/icons-material/FlashOn";
+import { IconButton } from "@mui/material";
 
 function SwipeButtons() {
   return (
-    <div>
-      <h2>Buttons Go Here</h2>
+    <div className="swipeButtons">
+      <IconButton className="swipeButtons__repeat">
+        <ReplayIcon fontSize="large" />
+      </IconButton>
+      <IconButton className="swipeButtons__left">
+        <CancelIcon fontSize="large" />
+      </IconButton>
+      <IconButton className="swipeButtons__star">
+        <StarRateIcon fontSize="large" />
+      </IconButton>
+      <IconButton className="swipeButtons__favorite">
+        <FavoriteIcon fontSize="large" />
+      </IconButton>
+      <IconButton className="swipeButtons__lightning">
+        <FlashOnIcon fontSize="large" />
+      </IconButton>
     </div>
   );
 }
