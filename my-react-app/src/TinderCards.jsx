7e8c4f6c32e27a18c2d8aa086d3407d276662822
Joshua Karasik <joshuakarasik@gmail.com>
2024-10-28 19:07:19 -0700

    commit

--- my-react-app/src/TinderCards.jsx
+++ my-react-app/src/TinderCards.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { useState } from "react";
 import TinderCard from "react-tinder-card";
+import "./TinderCards.css";
 
 export default function TinderCards() {
   const [people, setPeople] = useState([
     {
-      name: "steve jobs",
+      name: "Steve Jobs",
       url: "https://upload.wikimedia.org/wikipedia/commons/b/b9/Steve_Jobs_Headshot_2010-CROP.jpg",
     },
     {
-      name: "mark zuckerberg",
+      name: "Mark Zuckerberg",
       url: "https://upload.wikimedia.org/wikipedia/commons/f/fe/Mark_Zuckerberg_em_setembro_de_2014.jpg",
     },
   ]);
@@ -17,19 +18,22 @@ export default function TinderCards() {
   return (
     <div>
       <h1>Tinder Cards</h1>
-      {people.map((person) => (
-        <TinderCard
-          className="swipe"
-          key={person.name}
-        >
-          <div
-            className="card"
-            style={{ backgroundImage: `url(${person.url})` }}
+      <div className="tinderCards__cardContainer">
+        {people.map((person) => (
+          <TinderCard
+            className="swipe"
+            key={person.name}
+            preventSwipe={["up", "down"]}
           >
-            <h3>{person.name}</h3>
-          </div>
-        </TinderCard>
-      ))}
+            <div
+              className="card"
+              style={{ backgroundImage: `url(${person.url})` }}
+            >
+              <h3>{person.name}</h3>
+            </div>
+          </TinderCard>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ my-react-app/src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBvW5yztDZVWLjssuvfY_CWKqpHneXzZ98",
+  authDomain: "clone-47521.firebaseapp.com",
+  projectId: "clone-47521",
+  storageBucket: "clone-47521.appspot.com",
+  messagingSenderId: "1036492225679",
+  appId: "1:1036492225679:web:dc48867a3d27321a4f04fa",
+  measurementId: "G-4ZLL6JB241",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const database = firebaseApp.firestore();
