598758335b2851855556c3d67333c015c6fea271
Joshua Karasik <joshuakarasik@gmail.com>
2024-10-29 13:26:33 -0700

    new file

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./Header";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TinderCards from "./TinderCards";
+import SwipeButtons from "./SwipeButtons";
 
 function App() {
   return (
@@ -12,7 +13,12 @@ function App() {
         <Routes>
           <Route
             path="/"
-            element={<TinderCards />}
+            element={
+              <>
+                <TinderCards />
+                <SwipeButtons />
+              </>
+            }
           />
         </Routes>
       </Router>
---
+++ my-react-app/src/SwipeButtons.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SwipeButtons() {
+  return (
+    <div>
+      <h2>Buttons Go Here</h2>
+    </div>
+  );
+}
+
+export default SwipeButtons;
