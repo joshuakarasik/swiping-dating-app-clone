104b414e4270c96e9268fd33d51e57dea6b8fb28
Joshua Karasik <joshuakarasik@gmail.com>
2024-10-30 13:14:28 -0700

    css

--- my-react-app/src/Chat.jsx
+++ my-react-app/src/Chat.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import Avatar from "@mui/material/Avatar";
 
 function Chat({ name, message, profilePic, timestamp }) {
   return (
     <div className="chat">
-      <AccountCircleIcon
-        className="chat__image"
-        alt={name}
-        src={profilePic}
-      />
+      <div className="chat__avatarContainer">
+        <Avatar
+          className="chat__image"
+          alt={name}
+          src={profilePic}
+        />
+      </div>
+      <div>
+        <h2 className="chat__name">{name}</h2>
+        <p>{message}</p>
+      </div>
+      <p>{timestamp}</p>
     </div>
   );
 }
