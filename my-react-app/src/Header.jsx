41f57532d2ebd7d560dfc960666212243aeda338
Joshua Karasik <joshuakarasik@gmail.com>
2024-10-30 12:35:58 -0700

    back button

--- my-react-app/src/Header.jsx
+++ my-react-app/src/Header.jsx
@@ -2,20 +2,32 @@ import React from "react";
 import "./Header.css";
 import PersonIcon from "@mui/icons-material/Person";
 import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
-import CloudCircleIcon from "@mui/icons-material/CloudCircle";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import { IconButton } from "@mui/material";
 import flame from "./flame.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 
 export default function Header() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  // Only show the back button on the "/chats" route
+  const showBackButton = location.pathname === "/chats";
+
   return (
     <div className="Header">
-      <IconButton>
-        <PersonIcon
-          className="header__icon"
-          fontSize="large"
-        />
-      </IconButton>
+      {showBackButton ? (
+        <IconButton onClick={() => navigate("/")}>
+          <ArrowBackIosIcon fontSize="large" />
+        </IconButton>
+      ) : (
+        <IconButton>
+          <PersonIcon
+            className="header__icon"
+            fontSize="large"
+          />
+        </IconButton>
+      )}
       <Link to="/">
         <img
           src={flame}
